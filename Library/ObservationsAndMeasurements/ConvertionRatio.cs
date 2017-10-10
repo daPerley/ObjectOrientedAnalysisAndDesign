d9c782e9f0b3f393bdab26053404dcbb0f47981f
Perly Gustafsson <contact@daperley.com>
2017-10-10 10:10:30 +0200

    Obesrvation tables added

---
+++ Library/ObservationsAndMeasurements/ConvertionRatio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AnalysisLibrary.ObservationsAndMeasurements
+{
+    class ConvertionRatio
+    {
+        public Guid Id { get; set; }
+        public decimal Ratio { get; set; }
+
+        public Guid FromUnitId { get; set; }
+        public Unit FromUnit { get; set; }
+
+        public Guid ToUnitId { get; set; }
+        public Unit ToUnit { get; set; }
+    }
+}
---
+++ Library/ObservationsAndMeasurements/Quantity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnalysisLibrary.ObservationsAndMeasurements
+{
+    class Quantity
+    {
+        public Guid Id { get; set; }
+        public Decimal Amount { get; set; }
+
+        public Guid UnitId { get; set; }
+        public Unit Unit { get; set; }
+    }
+}

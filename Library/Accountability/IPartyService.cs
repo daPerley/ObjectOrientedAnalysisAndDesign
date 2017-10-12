07e91938eb0ca5e5e84ba5d799947928356bec34
Perly Gustafsson <contact@daperley.com>
2017-10-12 10:09:28 +0200

    Unit test for PartyRepository

---
+++ Library/Accountability/IPartyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalysisLibrary.Accountability
+{
+    public interface IPartyService
+    {
+        Party Get(string term);
+    }
+}

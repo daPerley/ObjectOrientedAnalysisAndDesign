c0a8023c4363427de571076cdb8f975b45625626
Perly Gustafsson <contact@daperley.com>
2017-10-29 16:41:21 +0100

    rules

--- BengansLibrary/Set.cs
+++ BengansLibrary/Set.cs
@@ -5,8 +5,28 @@
         public int Id { get; set; }
         public int SetNumber { get; set; }
         public int GamePartyId { get; set; }
-        public int[] Rounds { get; set; } = new int[10];
-    }
+        public int[] Frames { get; set; } = new int[10] { 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017 };
 
-    //Point calculator
+        // Strike = 10 + next two balls
+        // Spare = x+x=10 + next ball
+        //public static Set FrameCalculator(Set set, int firstBall, int secondBall, int bonusBall)
+        //{
+        //    foreach (var frame in set.Frames)
+        //    {
+        //        if (frame == 2017)
+        //        {
+        //            set.Frames[frame - 1] = firstBall + secondBall;
+
+        //            if (frame != 1 && set.Frames[frame - 2] == 10)
+        //            {
+        //                // only work for strikes, need to get it 
+        //                // see the diffrence between strikes and spares
+        //                set.Frames[frame - 2] += set.Frames[frame - 1];
+        //            }
+        //        }
+        //    }
+
+        //    return set;
+        //}
+    }
 }
